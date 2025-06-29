02985634e073bbb34f058b03cef9570a1780a55f
Nafiu Baba Saraki <153331190+NafiuSaraki@users.noreply.github.com>
2025-06-29 22:26:53 +0100

    Update axios.js

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://cinmate-backend.onrender.com/api",
 });
 
 // Add token to headers if it exists
