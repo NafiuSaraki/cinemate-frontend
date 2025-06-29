8df469dca8ed1eee80e78bfb5792083d90c8c86f
Nafiu Baba Saraki <153331190+NafiuSaraki@users.noreply.github.com>
2025-06-29 22:33:42 +0100

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -9,7 +9,7 @@ const Login = () => {
   const handleLogin = async (e) => {
     e.preventDefault();
     try {
-      const res = await axios.post('https://cinemate-frontend.onrender.com/api/login', {
+      const res = await axios.post('https://cinmate-backend.onrender.com/api/login', {
         email,
         password,
       });
